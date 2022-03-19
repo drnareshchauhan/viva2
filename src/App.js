8b2b2b8d20575f7b7315f75e05dab2b9aa31c9b0
drnareshchauhan <drnareshchauhan@gmail.com>
2022-03-19 11:46:51 +0530

    google analytics

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from "./Home";
 import About from "./pages/About";
 import VivaOne from "./pages/VivaOne";
 import VivaTwo from "./pages/VivaTwo";
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-163903670-2");
+ReactGA.pageview(window.location.pathname + window.location.search);
 const App = () => {
   return (
     <Routes>
